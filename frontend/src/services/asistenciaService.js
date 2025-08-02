72b805aa98018dc1ed4c612ff0c601ef54f45db3
Octuuu <125304301+Octuuu@users.noreply.github.com>
2025-08-02 20:53:47 -0300

    ..

--- frontend/src/services/asistenciaService.js
+++ frontend/src/services/asistenciaService.js
@@ -2,3 +2,21 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
 
+// crear asistencia
+export const crearAsistencia = async (datos, token) => {
+  const res = await axios.post(`${API_URL}/asistencias`, 
+    datos, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    }
+  )
+
+  return res.data;
+}
+
+// traer la materia
+
+
+// traer el curso
+
