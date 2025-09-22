42f1bb63897a6a34ff2972c4a3b3c67a926d2930
Octuuu <125304301+Octuuu@users.noreply.github.com>
2025-09-22 13:18:51 -0300

    ..

--- backend/routes/inscripciones.routes.js
+++ backend/routes/inscripciones.routes.js
@@ -4,7 +4,9 @@ import {
   getInscripcionesAlumno,
   getAlumnosCurso,
   borrarInscripcion,
-  getTodasInscripciones
+  getTodasInscripciones,
+  darDeBajaInscripcionHandler,
+  reactivarInscripcionHandler
 } from '../controllers/inscripciones.controller.js';
 import { authenticate, authorize } from '../middlewares/auth.middleware.js';
 
@@ -12,19 +14,13 @@ const router = express.Router();
 
 const rolesPermitidos = ['admin'];
 
-// Crear inscripción
 router.post('/', authenticate, authorize(rolesPermitidos), nuevaInscripcion);
-
-// Listar todas las inscripciones
 router.get('/todasInscripciones', authenticate, authorize(rolesPermitidos), getTodasInscripciones);
-
-// Obtener inscripciones de un alumno
 router.get('/alumno/:alumnoId', authenticate, authorize(rolesPermitidos), getInscripcionesAlumno);
-
-// Obtener alumnos de un curso
 router.get('/curso/:cursoId', authenticate, authorize(rolesPermitidos), getAlumnosCurso);
-
-// Eliminar inscripción
 router.delete('/:id', authenticate, authorize(rolesPermitidos), borrarInscripcion);
+router.patch('/:id/baja', darDeBajaInscripcionHandler);
+router.patch('/:id/reactivar', reactivarInscripcionHandler);
+
 
 export default router;
